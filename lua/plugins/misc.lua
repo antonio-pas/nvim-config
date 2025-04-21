464a2074615f9a370b7d93a38ea0e37306b192f0
antonio-pas <antonio.b.passos18@gmail.com>
2025-04-20 19:59:31 -0700

    Remove Markview, add Windsurf and VimTex

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -8,10 +8,24 @@ return {
     end,
     ft = { "markdown" },
   },
-  -- For `plugins.lua` users.
   {
-      "OXY2DEV/markview.nvim",
-      lazy = false
+    'Exafunction/windsurf.vim',
+    config = function ()
+      vim.g.codeium_disable_bindings = 1
+      vim.keymap.set('i', '<C-z>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-n>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-p>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
   },
+  {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      -- vim.g.vimtex_view_method = "zathura"
+    end
+  }
 }
 
